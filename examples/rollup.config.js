8c7ff722411df4e774def2844a5ae2cc1a5f26b1
Michael Stillwell <mjs@beebo.org>
2016-12-04 08:41:37 +0000

    Explain configuration

--- examples/rollup.config.js
+++ examples/rollup.config.js
@@ -3,7 +3,9 @@ import typescript from "rollup-plugin-typescript";
 export default {
   plugins: [
     typescript({
+      // Use locally-installed (newer) Typescript
       typescript: require("typescript"),
+      // Search parent directory for *.ts
       include: [ "../*.ts", "**/*.ts" ]
     })
   ]
