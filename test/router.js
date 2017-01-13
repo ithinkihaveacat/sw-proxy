1d314b9998ab7bf3cdc0f21c6132b93be5d4db05
Michael Stillwell <mjs@beebo.org>
2017-01-13 16:05:29 +0000

    Improve tests

--- test/router.js
+++ test/router.js
@@ -4,21 +4,43 @@ const assert = require('assert');
 
 describe('Router', () => {
   describe('#constructor()', () => {
-    it('should return a Router object (no routes)', () => {
+    it('should return a Router object (empty constructor)', () => {
       assert.ok(new Router() instanceof Router);
+    });
+    it('should return a Router object (no routes)', () => {
+      assert.ok(new Router({}) instanceof Router);
     });
     it('should return a Router object (one route)', () => {
       assert.ok(new Router({ "foo": "bar" }) instanceof Router);
-    })
+    });
   });
   describe('#match()', () => {
-    it('should return no matches (no routes)', () => {
+    it('should return no matches (empty constructor, no URL)', () => {
       const r = new Router();
       assert.deepEqual(r.match(), []);
     });
-    it('should return no matches (one route)', () => {
+    it('should return no matches (no routes, no URL)', () => {
+      const r = new Router({});
+      assert.deepEqual(r.match(), []);
+    });
+    it('should return no matches (one route, no URL)', () => {
       const r = new Router({ "foo": "bar" });
       assert.deepEqual(r.match(), []);
+    });
+    it('should return no matches (no routes, URL)', () => {
+      const r = new Router();
+      assert.deepEqual(r.match("qqq"), []);
+    });
+    it('should return no matches (one route, URL)', () => {
+      const r = new Router({ "foo": "bar" });
+      assert.deepEqual(r.match("qqq"), []);
+    });
+    it('should return one match (one route, URL)', () => {
+      const r = new Router({ "foo": "bar" });
+      assert.deepEqual(
+        r.match("foo"),
+        [["bar", "foo".match(new RegExp("foo"))]]
+      );
     });
   });
 });
