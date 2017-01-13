010f3ced4ce36f61ba37b80f037810045f5a7736
Michael Stillwell <mjs@beebo.org>
2017-01-13 17:46:18 +0000

    Even more tests

--- test/router.js
+++ test/router.js
@@ -44,12 +44,12 @@ describe('Router', () => {
     });
     it('should support prefix', () => {
       const r = new Router({ '/foo': 'bar' }, '/quux');
-      assert.ok(r.match('/quux/foo').length === 1);
+      assert.equal(r.match('/quux/foo').length, 1);
     });
     it('should match on regexp /foo/(.*)', () => {
       const r = new Router({ '/foo/(.*)': 'bar' });
       const m = r.match('/foo/43');
-      assert.ok(m.length === 1);
+      assert.equal(m.length, 1);
       assert.equal(m[0][0], 'bar');
       assert.equal(m[0][1][0], '/foo/43');
       assert.equal(m[0][1][1], '43');
@@ -57,10 +57,26 @@ describe('Router', () => {
     it('should match on regexp /foo/quux-(.*)/(\\d+)', () => {
       const r = new Router({ '/foo/quux-(.*)/(\\d+)': 'bar' });
       const m = r.match('/foo/quux-clem/54');
-      assert.ok(m.length === 1);
+      assert.equal(m.length, 1);
       assert.equal(m[0][0], 'bar');
       assert.equal(m[0][1][1], 'clem'); // first capture
       assert.equal(m[0][1][2], '54'); // second capture
+    });
+    it('should return a single match', () => {
+      const r = new Router({ 'foo1': 'bar1', 'foo2': 'bar2' });
+      const m1 = r.match('foo1');
+      assert.equal(m1.length, 1);
+      assert.equal(m1[0][0], 'bar1');
+      const m2 = r.match('foo2');
+      assert.equal(m2.length, 1);
+      assert.equal(m2[0][0], 'bar2');
+    });
+    it('should return two matches', () => {
+      const r = new Router({ 'foo': 'bar', '.*': 'baz' });
+      const m = r.match('foo');
+      assert.equal(m.length, 2);
+      assert.equal(m[0][0], 'bar');
+      assert.equal(m[1][0], 'baz');
     });
   });
 });
