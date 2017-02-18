3eb49f36b3d186a77d44f90ad48904d4e16052b5
Michael Stillwell <mjs@beebo.org>
2017-02-18 10:11:27 +0000

    File doesn't exist anymore

--- examples/register-ready.ts
+++ examples/register-ready.ts
@@ -12,8 +12,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-import "../service-worker";
-
 // Returns a `Promise<ServiceWorkerRegistration>` like
 /* tslint:disable-next-line:max-line-length */
 // [ServiceWorkerContainer.register()](https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerContainer/register),
